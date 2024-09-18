5cea3920412b313828b1d58b8f262bab49997eff
chanLik1208-dev <chanLik1208@gmail.com>
2024-09-18 21:34:52 +0800

    Update KeepTeamPlugin.java

--- src/example/KeepTeamPlugin.java
+++ src/example/KeepTeamPlugin.java
@@ -1,42 +1,34 @@
-import arc.Events;
-import arc.struct.ObjectMap;
-import arc.util.Log;
 import mindustry.mod.Plugin;
-import mindustry.game.EventType.PlayerJoin;
-import mindustry.game.EventType.PlayerLeave;
-import mindustry.game.Team;
-import mindustry.gen.Groups;
 import mindustry.gen.Player;
+import mindustry.net.Administration;
 
-public class KeepTeamPlugin extends Plugin {
+import java.util.HashMap;
+import java.util.Map;
 
-    private ObjectMap<String, ObjectMap<String, Object>> playerData = new ObjectMap<>();
+public class TeamPersistencePlugin extends Plugin {
+    private final Map<String, String> playerTeams = new HashMap<>();
 
     @Override
     public void init() {
+        // 监听玩家加入事件
         Events.on(PlayerJoin.class, event -> {
-            String uuid = event.player.uuid();
-            Log.info("Player @ joined", uuid);
-            ObjectMap<String, Object> data = getPlayerData(uuid);
-            if (data.containsKey("team")) {
-                int teamId = (int) data.get("team");
-                Player player = Groups.player.find(p -> p.uuid().equals(uuid));
-                if (player != null) {
-                    player.team(Team.get(teamId));
-                    Log.info("Player @ assigned to team @", uuid, teamId);
-                }
+            Player player = event.player;
+            String uuid = player.uuid();
+            if (playerTeams.containsKey(uuid)) {
+                player.team(Team.get(playerTeams.get(uuid)));
             }
         });
 
+        // 监听玩家离开事件
         Events.on(PlayerLeave.class, event -> {
-            String uuid = event.player.uuid();
-            ObjectMap<String, Object> data = getPlayerData(uuid);
-            data.put("team", event.player.team().id);
-            Log.info("Player @ left and their team @ was saved", uuid, event.player.team().id);
+            Player player = event.player;
+            playerTeams.put(player.uuid(), player.team().name);
         });
-    }
 
-    private ObjectMap<String, Object> getPlayerData(String uuid) {
-        return playerData.get(uuid, ObjectMap::new);
+        // 监听玩家更换队伍事件
+        Events.on(PlayerChangeTeamEvent.class, event -> {
+            Player player = event.player;
+            playerTeams.put(player.uuid(), event.newTeam.name);
+        });
     }
 }
