153bf5d79a58092d970e762605602196a19be431
chanLik1208-dev <chanLik1208@gmail.com>
2024-09-18 16:43:13 +0800

    Update KeepTeamPlugin.java

--- src/example/KeepTeamPlugin.java
+++ src/example/KeepTeamPlugin.java
@@ -12,19 +12,17 @@ public class KeepTeamPlugin extends Plugin {
     public void init() {
         // 监听玩家加入事件
         Events.on(PlayerJoin.class, event -> {
-            player = event.player;
             // 检查玩家是否有之前的队伍记录
-            if (player.customData().has("team")) {
+            if (event.player.customData().has("team")) {
                 // 将玩家分配到之前的队伍
-                player.team(Team.get(player.customData().getInt("team")));
+                event.player.team(Team.get(player.customData().getInt("team")));
             }
         });
 
         // 监听玩家离开事件
         Events.on(PlayerLeave.class, event -> {
-            player = event.player;
             // 保存玩家的队伍信息
-            player.customData().put("team", player.team().id);
+            event.player.customData().put("team", player.team().id);
         });
     }
 }
