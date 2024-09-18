f043b5a6d752a4e6158503e582ac49aa3d26496a
chanLik1208-dev <chanLik1208@gmail.com>
2024-09-18 19:35:08 +0800

    Update KeepTeamPlugin.java

--- src/example/KeepTeamPlugin.java
+++ src/example/KeepTeamPlugin.java
@@ -1,37 +1,43 @@
 import mindustry.mod.Plugin;
+import arc.Events;
+import arc.struct.ObjectMap;
+import arc.util.Log;
 import mindustry.game.EventType.PlayerJoin;
 import mindustry.game.EventType.PlayerLeave;
-import arc.Events;
-import mindustry.Vars;
 import mindustry.game.Team;
-import arc.struct.ObjectMap;
+import mindustry.Vars;
 
-public class KeepTeamHandler {
+public class KeepTeamHandler extends Plugin {
 
     // 用于存储玩家自定义数据的ObjectMap
     private ObjectMap<String, ObjectMap<String, Object>> playerData = new ObjectMap<>();
 
-    public KeepTeamHandler() {
+    public KeepTeamHandler extends Plugin() {
         // 监听玩家加入事件
         Events.on(PlayerJoin.class, event -> {
-            Player player = event.player;
+            // 获取玩家UUID
+            String uuid = event.player.uuid();
             // 检查玩家是否有之前的队伍记录
-            if (getPlayerData(player).containsKey("team")) {
+            if (getPlayerData(uuid).containsKey("team")) {
                 // 将玩家分配到之前的队伍
-                player.team(Team.get((int) getPlayerData(player).get("team")));
+                int teamId = (int) getPlayerData(uuid).get("team");
+                Vars.playerGroup.find(p -> p.uuid().equals(uuid)).team(Team.get(teamId));
+                Log.info("Player @ joined and was assigned to team @", uuid, teamId);
             }
         });
 
         // 监听玩家离开事件
         Events.on(PlayerLeave.class, event -> {
-            Player player = event.player;
+            // 获取玩家UUID
+            String uuid = event.player.uuid();
             // 保存玩家的队伍信息
-            getPlayerData(player).put("team", player.team().id);
+            getPlayerData(uuid).put("team", event.player.team().id);
+            Log.info("Player @ left and their team was saved", uuid);
         });
     }
 
     // 获取玩家自定义数据的方法
-    private ObjectMap<String, Object> getPlayerData(Player player) {
-        return playerData.get(player.uuid(), ObjectMap::new);
+    private ObjectMap<String, Object> getPlayerData(String uuid) {
+        return playerData.get(uuid, ObjectMap::new);
     }
 }
