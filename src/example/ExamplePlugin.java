bc1a87be02b01f2cd2295d105633e43a42afd8dd
chanLik1208-dev <chanLik1208@gmail.com>
2024-09-18 16:29:19 +0800

    Update ExamplePlugin.java

--- src/example/ExamplePlugin.java
+++ src/example/ExamplePlugin.java
@@ -1,40 +1,26 @@
-package example;
-
 import mindustry.mod.Plugin;
 import mindustry.gen.Player;
 import mindustry.net.Administration;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class TeamPersistencePlugin extends Plugin {
-    // 存储玩家队伍信息的映射
-    private Map<String, Integer> playerTeams = new HashMap<>();
+public class KeepTeamPlugin extends Plugin {
 
     @Override
     public void init() {
         // 监听玩家加入事件
         Events.on(PlayerJoin.class, event -> {
             Player player = event.player;
-            String uuid = player.uuid();
-
-            // 检查玩家是否有之前的队伍信息
-            if (playerTeams.containsKey(uuid)) {
-                // 设置玩家的队伍
-                player.team(Team.get(playerTeams.get(uuid)));
-            } else {
-                // 如果没有记录，则将玩家分配到默认队伍并记录
-                playerTeams.put(uuid, player.team().id);
+            // 检查玩家是否有之前的队伍记录
+            if (player.customData().has("team")) {
+                // 将玩家分配到之前的队伍
+                player.team(Team.get(player.customData().getInt("team")));
             }
         });
 
         // 监听玩家离开事件
         Events.on(PlayerLeave.class, event -> {
             Player player = event.player;
-            String uuid = player.uuid();
-
             // 保存玩家的队伍信息
-            playerTeams.put(uuid, player.team().id);
+            player.customData().put("team", player.team().id);
         });
     }
 }
