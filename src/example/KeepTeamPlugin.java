1c8eb3349565467b3cdd6d26d41d8c0fceee36b3
chanLik1208-dev <chanLik1208@gmail.com>
2024-09-18 21:40:07 +0800

    Update KeepTeamPlugin.java

--- src/example/KeepTeamPlugin.java
+++ src/example/KeepTeamPlugin.java
@@ -1,39 +1,38 @@
 import mindustry.mod.Plugin;
 import mindustry.gen.Player;
-import mindustry.net.Administration;
-import mindustry.game.Team;
+import arc.util.Log;
+import arc.Events;
 import mindustry.game.EventType.PlayerJoin;
 import mindustry.game.EventType.PlayerLeave;
-import mindustry.game.EventType.PlayerChangeTeamEvent;
-import arc.Events;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class TeamPersistencePlugin extends Plugin {
+public class TeamSavePlugin extends Plugin {
+    // 用於保存玩家隊伍信息的地圖
     private final Map<String, String> playerTeams = new HashMap<>();
 
     @Override
     public void init() {
-        // 监听玩家加入事件
+        // 監聽玩家加入事件
         Events.on(PlayerJoin.class, event -> {
             Player player = event.player;
             String uuid = player.uuid();
             if (playerTeams.containsKey(uuid)) {
-                player.team(Team.get(playerTeams.get(uuid)));
+                // 恢復玩家隊伍
+                String team = playerTeams.get(uuid);
+                player.team(Team.valueOf(team));
+                Log.info("恢復玩家 @ 的隊伍為 @", player.name, team);
             }
         });
 
-        // 监听玩家离开事件
+        // 監聽玩家離開事件
         Events.on(PlayerLeave.class, event -> {
             Player player = event.player;
-            playerTeams.put(player.uuid(), player.team().name);
-        });
-
-        // 监听玩家更换队伍事件
-        Events.on(PlayerChangeTeamEvent.class, event -> {
-            Player player = event.player;
-            playerTeams.put(player.uuid(), event.newTeam.name);
+            String uuid = player.uuid();
+            // 保存玩家隊伍信息
+            playerTeams.put(uuid, player.team().name());
+            Log.info("保存玩家 @ 的隊伍為 @", player.name, player.team().name());
         });
     }
 }
