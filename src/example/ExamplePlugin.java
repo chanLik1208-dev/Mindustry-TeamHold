2fd28191bad5d701e68dccbc0541410a59712305
chanLik1208-dev <chanLik1208@gmail.com>
2024-09-18 16:23:05 +0800

    Update ExamplePlugin.java

--- src/example/ExamplePlugin.java
+++ src/example/ExamplePlugin.java
@@ -1,84 +1,40 @@
 package example;
 
-import arc.*;
-import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
-import mindustry.gen.*;
-import mindustry.mod.*;
-import mindustry.net.Administration.*;
-import mindustry.world.blocks.storage.*;
+import mindustry.mod.Plugin;
+import mindustry.gen.Player;
+import mindustry.net.Administration;
 
-public class ExamplePlugin extends Plugin{
+import java.util.HashMap;
+import java.util.Map;
 
-    //called when game initializes
+public class TeamPersistencePlugin extends Plugin {
+    // 存储玩家队伍信息的映射
+    private Map<String, Integer> playerTeams = new HashMap<>();
+
     @Override
-    public void init(){
-        //listen for a block selection event
-        Events.on(BuildSelectEvent.class, event -> {
-            if(!event.breaking && event.builder != null && event.builder.buildPlan() != null && event.builder.buildPlan().block == Blocks.thoriumReactor && event.builder.isPlayer()){
-                //player is the unit controller
-                Player player = event.builder.getPlayer();
+    public void init() {
+        // 监听玩家加入事件
+        Events.on(PlayerJoin.class, event -> {
+            Player player = event.player;
+            String uuid = player.uuid();
 
-                //send a message to everyone saying that this player has begun building a reactor
-                Call.sendMessage("[scarlet]ALERT![] " + player.name + " has begun building a reactor at " + event.tile.x + ", " + event.tile.y);
+            // 检查玩家是否有之前的队伍信息
+            if (playerTeams.containsKey(uuid)) {
+                // 设置玩家的队伍
+                player.team(Team.get(playerTeams.get(uuid)));
+            } else {
+                // 如果没有记录，则将玩家分配到默认队伍并记录
+                playerTeams.put(uuid, player.team().id);
             }
         });
 
-        //add a chat filter that changes the contents of all messages
-        //in this case, all instances of "heck" are censored
-        Vars.netServer.admins.addChatFilter((player, text) -> text.replace("heck", "h*ck"));
+        // 监听玩家离开事件
+        Events.on(PlayerLeave.class, event -> {
+            Player player = event.player;
+            String uuid = player.uuid();
 
-        //add an action filter for preventing players from doing certain things
-        Vars.netServer.admins.addActionFilter(action -> {
-            //random example: prevent blast compound depositing
-            if(action.type == ActionType.depositItem && action.item == Items.blastCompound && action.tile.block() instanceof CoreBlock){
-                action.player.sendMessage("Example action filter: Prevents players from depositing blast compound into the core.");
-                return false;
-            }
-            return true;
-        });
-    }
-
-    //register commands that run on the server
-    @Override
-    public void registerServerCommands(CommandHandler handler){
-        handler.register("reactors", "List all thorium reactors in the map.", args -> {
-            for(int x = 0; x < Vars.world.width(); x++){
-                for(int y = 0; y < Vars.world.height(); y++){
-                    //loop through and log all found reactors
-                    //make sure to only log reactor centers
-                    if(Vars.world.tile(x, y).block() == Blocks.thoriumReactor && Vars.world.tile(x, y).isCenter()){
-                        Log.info("Reactor at @, @", x, y);
-                    }
-                }
-            }
-        });
-    }
-
-    //register commands that player can invoke in-game
-    @Override
-    public void registerClientCommands(CommandHandler handler){
-
-        //register a simple reply command
-        handler.<Player>register("reply", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
-            player.sendMessage("You said: [accent] " + args[0]);
-        });
-
-        //register a whisper command which can be used to send other players messages
-        handler.<Player>register("whisper", "<player> <text...>", "Whisper text to another player.", (args, player) -> {
-            //find player by name
-            Player other = Groups.player.find(p -> p.name.equalsIgnoreCase(args[0]));
-
-            //give error message with scarlet-colored text if player isn't found
-            if(other == null){
-                player.sendMessage("[scarlet]No player by that name found!");
-                return;
-            }
-
-            //send the other player a message, using [lightgray] for gray text color and [] to reset color
-            other.sendMessage("[lightgray](whisper) " + player.name + ":[] " + args[1]);
+            // 保存玩家的队伍信息
+            playerTeams.put(uuid, player.team().id);
         });
     }
 }
